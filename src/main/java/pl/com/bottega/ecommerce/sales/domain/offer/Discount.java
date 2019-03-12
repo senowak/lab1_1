23fc9489cfd3a9c67100c50253363a3e60ec2167
, <202249@169.254.16.200>
2019-03-12 18:39:27 +0100

    Dodana funkcjonalnosc klasy Discount

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,6 +1,27 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
 
 public class Discount {
 
+    private String cause;
+
+    Money value;
+
+    public Discount(BigDecimal kasa) {
+        this(kasa, null);
+    }
+
+    public Discount(BigDecimal kasa, String cause) {
+        this.value = new Money(kasa);
+        this.cause = cause;
+    }
+
+    public Money getValue() {
+        return value;
+    }
+
+    public String getCause() {
+        return cause;
+    }
 }
