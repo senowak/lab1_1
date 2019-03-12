2190aa39e23c0afb52dd6da203ab3d0d217daac6
, <202249@169.254.16.200>
2019-03-12 18:19:23 +0100

    Uzupełnienie klasy Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -1,6 +1,27 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
 
 public class Money {
 
+    private BigDecimal value;
+
+    private String currency;
+
+    public Money(BigDecimal value) {
+        this(value, "PLN");
+    }
+
+    public Money(BigDecimal value, String curr) {
+        this.value = value;
+        this.currency = curr;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
 }
